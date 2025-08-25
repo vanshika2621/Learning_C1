67c06abb79fcb204b60caa50690aaf6e7e200001
vanshika2621 <vanshikagiri2020@gmail.com>
2025-08-25 13:46:32 +0530

    traingle assignment using if and else

---
+++ triangle.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main()
+{ 
+    float s1 = 0.0, s2 = 0.0, s3 = 0.0; 
+    
+    printf("Enter all three sides: ");
+    scanf("%f", &s1);
+    scanf("%f", &s2);
+    scanf("%f", &s3); 
+    
+    // Check triangle validity
+    if (s1 + s2 > s3 && s1 + s3 > s2 && s2 + s3 > s1) 
+    { 
+        printf("The given triangle is valid\n"); 
+          
+        if (s1 == s2 && s2 == s3) 
+        {
+            printf("The triangle is Equilateral\n");
+        }
+        else if (s1 == s2 || s2 == s3 || s1 == s3) 
+        {
+            printf("The triangle is Isosceles\n");
+        }
+        else 
+        { 
+            printf("The triangle is Scalene\n"); 
+        }
+
+        if ( (s1*s1 + s2*s2 == s3*s3) || 
+             (s1*s1 + s3*s3 == s2*s2) || 
+             (s2*s2 + s3*s3 == s1*s1) ) 
+        { 
+            printf("The triangle is Right Angled\n");
+        } 
+    } 
+    else
+    {
+        printf("The given triangle is Invalid\n");
+    }
+
+    return 0; 
+}
