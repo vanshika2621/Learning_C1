f196f4272387ffc6c5c9ca2e3b4bc73f391ca1d7
vanshika2621 <vanshikagiri2020@gmail.com>
2025-09-12 13:11:55 +0530

    Update D15 sum of series.c

--- D15 sum of series.c
+++ D15 sum of series.c
@@ -1,19 +1,20 @@
 /*Write a program to find the sum of the series: 2/3 + 4/7 + 6/11 + 8/15 + ... up to n terms*/
+#include <stdio.h>
 
-#include<stdio.h> 
 int main()
 {
-    float  sum = 0.0 ; 
-    int num ;
+    float sum = 0.0; 
+    int num;
+
     printf("Enter the nth number: ");
-    scanf("%d", num) 
-    for (i = 1; i<= num , i++)
+    scanf("%d", &num);   // use &
+
+    for (int i = 1; i <= num; i++)
     {
         sum += (2.0 * i) / (4.0 * i - 1); 
-
     }
-    printf( "Approx number is = %.2f\n", n, sum);
 
-    return 0 ;
+    printf("Sum of the series up to %d terms = %.2f\n", num, sum);
 
+    return 0;
 }
