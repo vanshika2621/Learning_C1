5f8253a0d4c668033a2853c926685d18887439e4
vanshika2621 <vanshikagiri2020@gmail.com>
2025-09-14 11:57:12 +0530

    D17 pattern reverse2
    /*Write a program to print the following pattern:
    *****
    ****
    ***
    **
    * */

---
+++ D17 pattern reverse 2.c
@@ -0,0 +1,21 @@
+/*Write a program to print the following pattern:
+*****
+****
+***
+**
+* */
+
+#include<stdio.h>
+int main()
+{
+    int i , j ;
+    for (i=5 ; i>= 0 ; i--)
+    {
+        for(j = i ;j>= 0; j-- )
+        {
+            printf("*");
+        }
+    printf("\n"); 
+    }
+return 0 ;
+} 
