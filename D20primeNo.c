458e019f606fc8e85d3663adf66e519bbb9ced71
vanshika2621 <vanshikagiri2020@gmail.com>
2025-09-18 16:14:01 +0530

    D20nprime numbers
    Write a program to print all the prime numbers from 1 to n

---
+++ D20primeNo.c
@@ -0,0 +1,30 @@
+/*Write a program to print all the prime numbers from 1 to n*/
+
+
+#include <stdio.h>
+
+int main() {
+    int n, i, j, isPrime;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    printf("Prime numbers from 1 to %d are:\n", n);
+
+    for (i = 2; i <= n; i++) {   
+        isPrime = 1;             
+
+        for (j = 2; j * j <= i; j++) {
+            if (i % j == 0) {   
+                isPrime = 0;
+                break;
+            }
+        }
+
+        if (isPrime) {
+            printf("%d ", i);
+        }
+    }
+
+    return 0;
+}
