a4579f36581503d7c0bba5c5febab8d144f2369b
vanshika2621 <vanshikagiri2020@gmail.com>
2025-09-26 11:12:01 +0530

    D28que2

---
+++ D28que2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int num, i, isPrime = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num <= 1) {
+        isPrime = 0; // 0 and 1 are not prime
+    } else {
+        for (i = 2; i <= num / 2; i++) {
+            if (num % i == 0) {
+                isPrime = 0;
+                break;
+            }
+        }
+    }
+
+    if (isPrime == 1) {
+        printf("%d is a Prime number.\n", num);
+    } else {
+        printf("%d is NOT a Prime number.\n", num);
+    }
+
+    return 0;
+}
