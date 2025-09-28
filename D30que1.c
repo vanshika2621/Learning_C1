5a34060c069cf4c941ae9a7432882504c07448a6
vanshika2621 <vanshikagiri2020@gmail.com>
2025-09-28 11:03:48 +0530

    D30 Que1

---
+++ D30que1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int n, sum = 0, i = 1;
+
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+
+    while (i <= n) {
+        sum += i;
+        i++;
+    }
+
+    printf("Sum of first %d natural numbers = %d\n", n, sum);
+    return 0;
+}
