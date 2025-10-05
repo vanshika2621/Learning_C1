567613edbc383b5769f435b542340028a48f2b05
vanshika2621 <vanshikagiri2020@gmail.com>
2025-10-05 18:25:22 +0530

    array

---
+++ arr.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    int arr[100], largest;
+
+    printf("Enter number of elements (max 100): ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    largest = arr[0];
+    for (i = 1; i < n; i++) {
+        if (arr[i] > largest) {
+            largest = arr[i];
+        }
+    }
+
+    printf("The largest element is: %d\n", largest);
+
+    return 0;
+}
