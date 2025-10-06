40a56a03588d85f5b01a71b263bea7b8c5421e02
vanshika2621 <vanshikagiri2020@gmail.com>
2025-10-06 15:57:57 +0530

    five

---
+++ five.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    float sum = 0, average;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];  // declaring array of size n
+
+    printf("Enter %d numbers:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+        sum += arr[i];
+    }
+
+    average = sum / n;
+
+    printf("\nSum = %.2f", sum);
+    printf("\nAverage = %.2f\n", average);
+
+    return 0;
+}
