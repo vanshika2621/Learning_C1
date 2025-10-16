49bf26e66d5dc948180cb28a64c40b8d314e6a9f
vanshika2621 <vanshikagiri2020@gmail.com>
2025-10-16 22:02:37 +0530

    Create hw.c

---
+++ hw.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    // printf is a function from the standard input/output library (stdio.h)
+    printf("Hello, World!\n");
+
+    // main function should return 0 to indicate successful execution
+    return 0;
+}
