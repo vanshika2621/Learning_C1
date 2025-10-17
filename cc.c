41fa7dc21accc46c91b5a4f988add00a27fdc3aa
vanshika2621 <vanshikagiri2020@gmail.com>
2025-10-17 10:14:26 +0530

    Create cc.c

---
+++ cc.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, sum;
+
+    // Input two numbers
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+
+    // Calculate sum
+    sum = num1 + num2;
+
+    // Display result
+    printf("Sum = %d\n", sum);
+
+    return 0;
+}
