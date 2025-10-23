498406968fddfa276b4c0830e47341d4bbbedba1
vanshika2621 <vanshikagiri2020@gmail.com>
2025-10-23 11:23:36 +0530

    Create str4.c

---
+++ str4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // Declare and initialize a string (a character array)
+    char myString[] = "Hello, C Programming!";
+
+    // Print the string
+    printf("The string is: %s\n", myString);
+
+    return 0;
+}
