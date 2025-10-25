90707a93322674f4399ce4ee8ff6af5fd3cb0101
vanshika2621 <vanshikagiri2020@gmail.com>
2025-10-25 18:28:35 +0530

    Create code1.c

---
+++ code1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // Declare and initialize two integer variables
+    int number1 = 10;
+    int number2 = 5;
+    
+    // Perform an addition and store the result
+    int sum = number1 + number2;
+    
+    // Print the result. %d is a format specifier for integers.
+    printf("The sum of %d and %d is: %d\n", number1, number2, sum); 
+    
+    return 0;
+}
