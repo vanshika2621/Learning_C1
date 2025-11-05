5d0c12a647be0b8a0d4d7c30db306edff95c270c
vanshika2621 <vanshikagiri2020@gmail.com>
2025-11-05 23:13:04 +0530

    Create 5nov.c

---
+++ 5nov.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main() {
+    int rows, cols;
+    
+    // Input rows and columns
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+    printf("Enter number of columns: ");
+    scanf("%d", &cols);
+
+    int matrix[10][10], transpose[10][10];
+
+    // Input matrix elements
+    printf("Enter elements of the matrix:\n");
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < cols; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    // Calculate transpose
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < cols; j++) {
+            transpose[j][i] = matrix[i][j];
+        }
+    }
+
+    // Display original matrix
+    printf("\nOriginal Matrix:\n");
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < cols; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    // Display transposed matrix
+    printf("\nTranspose of the Matrix:\n");
+    for(int i = 0; i < cols; i++) {
+        for(int j = 0; j < rows; j++) {
+            printf("%d ", transpose[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
