4f91206dc4d15d65622bb6dbb3c6facb962b86d1
vanshika2621 <vanshikagiri2020@gmail.com>
2025-11-08 11:59:57 +0530

    Create 8nov2.c

---
+++ 8nov2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int sumArray(int arr[], int n) {
+    int sum = 0;
+    for(int i = 0; i < n; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+int main() {
+    int n;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d elements:\n", n);
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int result = sumArray(arr, n);
+
+    printf("Sum of array elements = %d\n", result);
+
+    return 0;
+}
