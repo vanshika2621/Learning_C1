4994fbb4fff3c760d2db93d091a545b6a68ec360
vanshika2621 <vanshikagiri2020@gmail.com>
2025-11-13 21:04:22 +0530

    Create 13nov2.c

---
+++ 13nov2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, sum;
+    int *ptr1, *ptr2;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+
+    ptr1 = &num1;  // pointer to num1
+    ptr2 = &num2;  // pointer to num2
+
+    sum = *ptr1 + *ptr2;  // accessing values using pointers
+
+    printf("Sum = %d\n", sum);
+
+    return 0;
+}
