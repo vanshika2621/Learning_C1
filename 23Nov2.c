ca07aa0b94bb02ada6251aa500d3593bc7d2e5c3
vanshika2621 <vanshikagiri2020@gmail.com>
2025-11-23 23:51:15 +0530

    Create 23Nov2.c

---
+++ 23Nov2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    char str[100];
+    int vowels = 0, consonants = 0, i;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    for (i = 0; str[i] != '\0'; i++) {
+        char ch = tolower(str[i]);
+
+        if (ch >= 'a' && ch <= 'z') {
+            if (ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u')
+                vowels++;
+            else
+                consonants++;
+        }
+    }
+
+    printf("Vowels: %d\n", vowels);
+    printf("Consonants: %d\n", consonants);
+
+    return 0;
+}
