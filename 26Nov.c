b14555f79fb0f3bf602b5e5ce40f8319f647191b
vanshika2621 <vanshikagiri2020@gmail.com>
2025-11-26 18:54:56 +0530

    Create 26Nov.c

---
+++ 26Nov.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    char str[100];
+    int i, count = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    for (i = 0; str[i] != '\0'; i++) {
+        char ch = tolower(str[i]);
+        if (ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u') {
+            count++;
+        }
+    }
+
+    printf("Number of vowels: %d\n", count);
+
+    return 0;
+}
