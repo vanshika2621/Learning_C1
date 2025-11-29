55456b2e07f89c9dbc6b8407e7e9dedcffb64dd6
vanshika2621 <vanshikagiri2020@gmail.com>
2025-11-29 09:30:57 +0530

    Create 29novx2.c

---
+++ 29novx2.c
@@ -0,0 +1,15 @@
+//Find the sum of array elements.
+
+#include<stdio.h>
+int main() {
+    int a1[5] = {2,5,8,7,6} ;
+    int sum=0;
+    for(int i = 0; i<5 ; i++){
+        sum = sum + a1[1] ;
+        
+        
+    }
+    printf("%d\n", sum);
+    return 0;
+
+}
