5c3f54d876f9dcd4b7de507f339fe051e9dcdc85
vanshika2621 <vanshikagiri2020@gmail.com>
2025-11-30 10:55:59 +0530

    Create practicex7.c

---
+++ practicex7.c
@@ -0,0 +1,18 @@
+//Find the digit that occurs the most ti in an integer numbermes
+//DOUBTSSS!!!!!!!
+
+#include<stdio.h>
+int main(){
+    int a1[6];
+    int count=0; 
+
+    for(int i=0 ; i<6 ;i++){
+        printf("Enter %d the elements: ", i);
+        scanf("%d", &a1[i]);
+    }
+    for(int j =0; j<6; j++){
+        
+    }
+
+    return 0;
+}
